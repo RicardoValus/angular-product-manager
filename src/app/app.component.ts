0087b3264e4b4ae40428d0fb945ba844cccb7c5d
RicardoValus <ricardovalus@gmail.com>
2024-05-06 09:53:27 -0300

    v1.0

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/template/header/header.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent
+  ],
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+
+}
